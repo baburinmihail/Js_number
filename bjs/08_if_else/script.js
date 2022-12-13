48c6193d19b7f659434a0e5afe91355580c3dcd7
baburinmihail <baburinmihail223@gmail.com>
2022-12-13 12:16:36 +0700

    I finished replacing the numbers and checking for length

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,5 +1,6 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+TextCorection();
 
 //проверка вводных данных на тип
 if (isNaN(minValue) || isNaN(maxValue) ){
@@ -7,14 +8,22 @@ if (isNaN(minValue) || isNaN(maxValue) ){
     maxValue = 100;
 }
 //проверки вводных данных на ограничение(тернарный тип)
-(minValue<-999) ? (minValue=-999): (minValue = minValue);
-(maxValue>999) ? (maxValue=999): (maxValue = maxValue);
+(minValue < -999) ? (minValue = -999): (minValue = minValue);
+(maxValue > 999) ? (maxValue = 999): (maxValue = maxValue);
 
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 let questionText = 'Текст вопроса';
+let znak = ''
+let dlinnaText = 3;
+let sotkiTextLength = 0;
+let desytkiTextLength = 0;
+let edenitchTextLength = 0;
+let sotkiText = 'сотня';
+let desytkiText = 'десять';
+let edenitchText = 'один';
 
 
 //номер вопроса (5)
@@ -25,29 +34,11 @@ const answerField = document.getElementById('answerField');
 //Приравниваю к 1 и перевожу в текст
 orderNumberField.innerText = orderNumber;
 
-
-//функция вывода заготовленного вопроса
-function questionRandomfun(){
-    let questionRandom = Math.round( Math.random()*3);
-    switch(questionRandom){
-        case (1):  
-            questionText = 'Люк я твой отец';
-            break;
-        case (2):  
-            questionText = 'Угадал';
-            break;
-        case (3):  
-            questionText = 'Возможно это число';
-            break;    
-        default:
-            questionText = 'Не это';
-            break;
-    }
-}
+//функция случайного вопроса
 questionRandomfun();
 
-
-answerField.innerText = `${questionText}  ${answerNumber }?`;
+//функция объеденяющая 4 функци(знак и 3 разряда уже в текстовом виде) 
+combiningDigits();
 
 
 //перезапуск
@@ -129,5 +120,235 @@ document.getElementById('btnEqual').addEventListener('click', function () {
     }
 })
 
+//функция проверки коррекности данных
+function TextCorection(){
+    if (minValue>=maxValue){
+        alert('Данные введены не коректно');
+        location.reload();
+    }
+}
+
+//функция вывода заготовленного вопроса
+function questionRandomfun(){
+    let questionRandom = Math.round( Math.random()*3);
+    switch(questionRandom){
+        case (1):  
+            questionText = 'Люк я твой отец';
+            break;
+        case (2):  
+            questionText = 'Угадал';
+            break;
+        case (3):  
+            questionText = 'Возможно это число';
+            break;    
+        default:
+            questionText = 'Не это';
+            break;
+    }
+}
 
 
+//функция на сотни
+function numberToTextSotki(){
+    let sotki = Math.abs(answerNumber);
+    if ((sotki/100)>1){
+        sotki = Math.trunc(sotki/100);
+        switch(sotki){
+            case (1):  
+                sotkiText = 'сто';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;    
+            case (2):
+                sotkiText = 'двести';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (3):
+                sotkiText = 'триста';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (4):
+                sotkiText = 'четыриста';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (5):
+                sotkiText = 'пятьсот';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (6):
+                sotkiText = 'шесот';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (7):
+                sotkiText = 'семьсот';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (8):
+                sotkiText = 'восемьсот';
+                console.log(sotkiText);
+                sotkiTextLength = sotkiText.length;
+                break;
+            case (9):
+                sotkiText = 'девятьсот';
+                sotkiTextLength = sotkiText.length;
+                console.log(sotkiText);
+                break;    
+        }
+    }else{
+        sotkiText = ''
+        console.log(sotkiText);
+        sotkiTextLength = sotkiText.length;
+    }
+}
+
+//функция на десятки
+function numberToTextDesytki(){
+    let desytki = Math.abs(answerNumber);
+    if (desytki>9){
+        desytki = Math.trunc(((desytki%100)-(desytki%10))/10);
+        console.log(desytki)
+        switch(desytki){
+            case (0):  
+                desytkiText = '_';
+                console.log(desytkiText)
+                desytkiTextLength = desytkiText.length;
+                break;  
+            case (1):  
+                desytkiText = 'десять';
+                console.log(desytkiText)
+                desytkiTextLength = desytkiText.length;
+                break;    
+            case (2):
+                desytkiText = 'двадцать';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (3):
+                desytkiText = 'тридцать';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (4):
+                desytkiText = 'сорак';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (5):
+                desytkiText = 'пятьдесят';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (6):
+                desytkiText = 'шестьдесят';
+                console.log(desytkiText);
+                break;
+            case (7):
+                desytkiText = 'семдесят';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (8):
+                desytkiText = 'восемьдесят';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;
+            case (9):
+                desytkiText = 'девяносто';
+                console.log(desytkiText);
+                desytkiTextLength = desytkiText.length;
+                break;    
+        }
+    }else{
+        desytkiText = '_'
+        console.log(desytkiText)
+        desytkiTextLength = desytkiText.length
+    }
+}
+
+//функция на еденицы
+function numberToTextEdenitch(){
+    let edenitch = Math.abs(answerNumber);
+    if (edenitch>-1){
+        edenitch = Math.trunc(edenitch%10);
+        console.log(edenitch)
+        switch(edenitch){
+            case (0):  
+                edenitchText = '_';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;  
+            case (1):  
+                edenitchText = 'один';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;    
+            case (2):
+                edenitchText = 'два';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (3):
+                edenitchText = 'три';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (4):
+                edenitchText = 'четыри';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (5):
+                edenitchText = 'пять';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (6):
+                edenitchText = 'шесть';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (7):
+                edenitchText = 'семь';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (8):
+                edenitchText = 'восемь';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;
+            case (9):
+                edenitchText = 'девять';
+                console.log(edenitchText);
+                edenitchTextLength = edenitchText.length;
+                break;    
+        }
+    }else{
+        edenitchText = '_'
+        console.log(edenitchText);
+        edenitchTextLength = edenitchText.length;
+    }
+}
+
+//функция склеивания и проверки на динну символов
+function combiningDigits(){
+    (answerNumber>=0)?(znak = '+'):(znak = '-');
+    console.log(znak)
+    numberToTextSotki();
+    numberToTextDesytki();
+    numberToTextEdenitch();
+    dlinnaText = (sotkiTextLength + desytkiTextLength + edenitchTextLength);
+    console.log(dlinnaText);
+        //вывод результата
+        if (dlinnaText>20){
+            answerField.innerText = `${questionText}  ${answerNumber }?`;
+        }else{
+            answerField.innerText = `${questionText}  ${(sotkiText+desytkiText+edenitchText) }?`;
+        }
+}
